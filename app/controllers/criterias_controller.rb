36e3f40955b1f091005a3a654a3f844720aa93b1
Thomas Colombet <tom1.webdev@gmail.com>
2018-08-28 11:38:22 +0200

    controller & view init

---
+++ app/controllers/criterias_controller.rb
@@ -0,0 +1,10 @@
+class CriteriasController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :criterias, only: [:new, :create, :index]
+  resources :events, only: :show
+  resources :choices, only: [:index, :show, :create]
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
