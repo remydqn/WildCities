1ea2874044cd964582d609da4d4acb12ce222739
Megan <megan.debar@gmail.com>
2018-08-29 10:45:17 +0200

    migration debug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_142057) do
+ActiveRecord::Schema.define(version: 2018_08_28_134207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,8 +35,6 @@ ActiveRecord::Schema.define(version: 2018_08_28_142057) do
     t.integer "wild"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_criterias_on_user_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -78,6 +76,5 @@ ActiveRecord::Schema.define(version: 2018_08_28_142057) do
 
   add_foreign_key "choices", "events"
   add_foreign_key "choices", "users"
-  add_foreign_key "criterias", "users"
   add_foreign_key "users", "criterias"
 end
