8732872960d50db7b099885c47748ef834ff49e5
benjamin <benjolivot@gmail.com>
2018-08-29 16:48:30 +0200

    3 choices OK

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -3,28 +3,36 @@ class ChoicesController < ApplicationController
   end
 
   def new
-    @choice = Choice.new
     criteria = current_user.criteria
     if criteria.drink == true
-      @events = Event.where(event_type: "drink").page(params[:page]).per(1)
+      @event = Event.where(event_type: "drink").first
     elsif criteria.dance == true
-      @events = Event.where(event_type: "dance").page(params[:page]).per(1)
+      @event = Event.where(event_type: "dance").first
     elsif criteria.culture == true
-      @events = Event.where(event_type: "culture").page(params[:page]).per(1)
+      @event = Event.where(event_type: "culture").first
     end
+    @choice = Choice.create(event: @event, user: current_user)
   end
 
-  def create
-    @choice = choice.new(choices_params)
-    @choice.user = current_user
-    @choice.event = Choice.find(params[:choice_id])
-    @choice.save
-    redirect_to choice_path(@choice)
+  # PUT /choices/:id
+  def update
+    @choice = current_user.choices.find(params[:id])
+
+    @choice.update(choice_params)
+    if current_user.choices.where(accepted: true).count == 3
+     redirect_to root_path
+    else
+      redirect_to new_choice_path
+    end
+    # @choice.user = current_user
+    # @choice.event = Choice.find(params[:choice_id])
+    # @choice.save
+    # redirect_to choice_path(@choice)
   end
 
   private
 
   def choice_params
-    params.require(:choice).permit(:name)
+    params.require(:choice).permit(:accepted)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   belongs_to :criteria
+  has_many :choices
 
   before_validation :create_criteria
 
