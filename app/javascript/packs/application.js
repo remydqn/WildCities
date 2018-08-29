4b2950538a6f4752f9d5cdf0fa4fcae18fe69627
remy duquenne <duquenneremy@gmail.com>
2018-08-29 16:50:57 +0200

    opacity image home page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,3 @@
 import "bootstrap";
-import SmoothScroll from "smooth-scroll";
-
-// typed.js
-
-import { loadDynamicBannerText } from '../../assets/javascripts/components/home';
-loadDynamicBannerText();
-
-// scroll down
-
-new SmoothScroll('a[href*="#"]');
-
-
+import "./smooth-scroll";
+import "./typed";
