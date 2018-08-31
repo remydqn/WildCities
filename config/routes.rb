e7165a3b9e667d730960571f7407330110bd0c8c
benjamin <benjolivot@gmail.com>
2018-08-31 17:12:19 +0200

    changement strategique

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
 
   resource :criterias, only: [:edit, :update]
   resources :events, only: [:show, :index]
-  resources :choices, only: [:index, :new, :update]
+  resources :choices, only: [:index, :new, :update] do
+    member do
+      patch :accept
+      patch :decline
+    end
+  end
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
