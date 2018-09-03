d0b7fa2a4001400ff7774c082a5dc38e37d6071b
Thomas Colombet <tom1.webdev@gmail.com>
2018-09-03 12:05:06 +0200

    loader init

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,11 @@
 import "bootstrap";
 import "./smooth-scroll";
 import "./typed";
+
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(loaderRemove, 2000);
+});
+
+function loaderRemove() {
+  document.querySelector("#loader").remove()
+}
