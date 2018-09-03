ce67cfd2155e1e209ccbd8aba6e59705affbd863
Megan <megan.debar@gmail.com>
2018-09-03 10:27:21 +0200

    new seeds drink dance

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,181 +39,190 @@ thomas = User.create!(
 puts User.all
 
 
-
+Event.create(
+  name: "Hot Stuff, Terminus Club",
+  normal_description: "Antinote's boss Zaltan and Sentiments—one of the most interesting French producers right now—join forces to finish the week with good vibes, in a backroom of an Italian restaurant! ",
+  address: "3 rue Terme, 69001 Lyon",
+  event_type: "dance",
+  wild: 6,
+  explained_description: "You want to dance all night ? Give the Password to the boss !",
+  tips: "Backroom, Password : ZALTAN2018",
+  pending: true,
+  city: "Lyon",
+  secret_event: false
+)
 
 
 Event.create(
-  name: "Event Drink",
-  normal_description: "This is a Drink Event",
-  address: "Britannia, Part-Dieu",
+  name: "Secret Poker night on a rooftop !",
+  normal_description: "Come and play poker with people during a night on a rooftop renting by bar servers ! Openbar and music all night.",
+  address: "27 rue du Commandant Lamperière, 69007 Lyon",
   event_type: "drink",
-  wild: 2,
-  explained_description: "This is a Drink Event in Part-Dieu !",
-  tips: "Stage 2, Bloc B",
+  wild: 6,
+  explained_description: "Starting bet : 20 euros !",
+  tips: "On the roof, take the fire escape at the back of the building !",
   pending: true,
   city: "Lyon",
   secret_event: false
 )
 
 Event.create(
-  name: "Event Drink 2",
-  normal_description: "This is a Drink Event 2",
-  address: "marseille",
-  event_type: "drink",
-  wild: 2,
-  explained_description: "This is a Drink Event 2 in Part-Dieu !",
-  tips: "Stage 2, Bloc B",
+  name: "Disused shed, transe music and people from everywhere !",
+  normal_description: "",
+  address: "148 avenue Berthelot, 69007 Lyon",
+  event_type: "dance",
+  wild: 6,
+  explained_description: "Discover the most secret (and illegal) spot to listen transe music all night. You can pay the entry price with what you want !",
+  tips: "Be ready to run if cops show up !",
   pending: true,
   city: "Lyon",
   secret_event: false
 )
 
 Event.create(
-  name: "Event Drink 3",
-  normal_description: "This is a Drink Event 3",
-  address: "Britannia, Part-Dieu",
+  name: "La Favela Chic",
+  normal_description: "Reach a secret room via the back of the theater, on the other side of the red curtain.",
+  address: "57 avenue Léon Blum, 69100 Villeurbanne",
   event_type: "drink",
-  wild: 3,
-  explained_description: "This is a Drink Event 3 in Part-Dieu !",
-  tips: "Stage 3, Bloc B",
+  wild: 6,
+  explained_description: "This place combined at the same time a dancefloor with pleasant and Brazilian musics and a bar with a beautiful New York decoration !",
+  tips: "Come on in by the back entrance.",
   pending: true,
   city: "Lyon",
   secret_event: false
 )
+
+Event.create(
+  name: "Lets dance in a garage !",
+  normal_description: "This is not only a garage.. after 20h, when the doors are closed, the night begin! Music, cocktails.. in the secret place in the huge room at the bottom of the garage.",
+  address: "12-14 rue Louis blanc, 69006 Lyon",
+  event_type: "dance",
+  wild: 6,
+  explained_description: "If you go outside of the garage, you can't come back inside!",
+  tips: "You have to know the boss' name : Richard",
+  pending: true,
+  city: "Lyon",
+  secret_event: false
+)
+
+Event.create(
+  name: "La Daronne",
+  normal_description: "This place is unique! You have to get your pass, it's 2 euros. After that, you will drink and dance all night In an associative bar where each people can expose his artistic talent, whatever it is!",
+  address: "23 quai Jean-Moulin, 69001 Lyon",
+  event_type: "drink",
+  wild: 6,
+  explained_description: "Be ready to show your artistic talent too!",
+  tips: "Don't forget your pass! And always say Hello to Chrstina, the boss.",
+  pending: true,
+  city: "Lyon",
+  secret_event: false
+)
+
+
+Event.create(
+  name: "Under the Bridge",
+  normal_description: "Improvised and short-lived evening under the bridge in front of the Cité Internationale. Campfire, music, urbex, beers and freedom all night long.",
+  address: "Promenade du bas Rhône, 69006 Lyon",
+  event_type: "drink",
+  wild: 6,
+  explained_description: "Cool and wild, in the nature, near the river.. !",
+  tips: "Do not come unfortified.",
+  pending: true,
+  city: "Lyon",
+  secret_event: false
+)
+
 
 puts "- Events Dance..."
 
 Event.create(
-  name: "Event Dance",
-  normal_description: "This is a Dance Event",
-  address: "Britannia, Part-Dieu",
+  name: "La Taverne de l'Ours qui boit",
+  normal_description: "The perfect place to combine The discovery of still unknown artists and a huge dancefloor with Indie/Pop music.",
+  address: "15 rue Magenta, 69006 Lyon",
   event_type: "dance",
-  wild: 2,
-  explained_description: "This is a Dance Event in Part-Dieu !",
-  tips: "Stage 2, Bloc B",
+  wild: 6,
+  explained_description: "Open your mind, move your body, drink again and again and bet your money on paintings.",
+  tips: "Look for the Red Door and knock.",
   pending: true,
   city: "Lyon",
   secret_event: false
 )
 
+
+
 Event.create(
-  name: "Event Dance 2",
-  normal_description: "This is a Dance Event 2",
-  address: "Britannia, Part-Dieu",
+  name: "The EGRAB",
+  normal_description: "You've heard about the barges in Lyon ? We will take you on an outstanding barge, a different barge. ",
+  address: "Place de Montréal, 69002 Lyon",
   event_type: "dance",
-  wild: 1,
-  explained_description: "This is a Dance Event 2 in Part-Dieu !",
-  tips: "Stage 2, Bloc B",
+  wild: 6,
+  explained_description: " This barge is crazy. I can't tell you more, you have to see this by your own!",
+  tips: "Cross the little park to find the barge, by the river.",
   pending: true,
   city: "Lyon",
   secret_event: false
 )
 
+
+
 Event.create(
-  name: "Event Dance 3",
-  normal_description: "This is a Dance Event 3",
-  address: "Britannia, Part-Dieu",
+  name: "Melange",
+  normal_description: "This is the place where people come from everywhere, every social class. ",
+  address: "1 rue Auguste Payant, 69008 Lyon",
   event_type: "dance",
-  wild: 2,
-  explained_description: "This is a Dance Event 3 in Part-Dieu !",
-  tips: "Stage 2, Bloc B",
+  wild: 6,
+  explained_description: "No shame, no judgement, no insistent look, just come as you are!",
+  tips: "Prepare yourself to take off one of your clothes at the entrance.",
   pending: true,
   city: "Lyon",
   secret_event: false
 )
 
+
+
 Event.create(
-  name: "Event Dance 4",
-  normal_description: "This is a Dance Event 4",
-  address: "Britannia, Part-Dieu",
+  name: "LAVOMATIC",
+  normal_description: "A funtionnal lavomatic hiding a secret bar.",
+  address: "56 rue Domer, 69007 Lyon",
   event_type: "dance",
-  wild: 2,
-  explained_description: "This is a Dance Event 4 in Part-Dieu !",
-  tips: "Stage 2, Bloc B",
+  wild: 6,
+  explained_description: "Behind washing machines - You just got to press on the button concealed to open a hidden door - hides a staircase which leads directly towards a kind of small apartment, with smoking room. A cosy place where we drink cocktail.",
+  tips: "Press the red button behing the 5th whashing machine.",
   pending: true,
   city: "Lyon",
   secret_event: false
 )
+
+
 
 Event.create(
-  name: "Event Dance 5",
-  normal_description: "This is a Dance Event 5",
-  address: "Britannia, Part-Dieu",
-  event_type: "dance",
-  wild: 2,
-  explained_description: "This is a Dance Event 5 in Part-Dieu !",
-  tips: "Stage 2, Bloc B",
+  name: "Le Syndicat",
+  normal_description: "A high-end cocktail establishment in a poor neighborhood. Come and enjoy atypical and varied cocktails in a bar with an underground spirit specialized in French alcohol.",
+  address: "",
+  event_type: "drink",
+  wild: 6,
+  explained_description: "The most doubtful bar in Lyon! Don't be afraid to come inside.. even if the outside is weird.",
+  tips: "45 rue Parmentier, 69200 Vénissieux",
   pending: true,
   city: "Lyon",
   secret_event: false
 )
 
-puts "- Events Culture..."
-
-Event.create(
-  name: "Event Culture",
-  normal_description: "This is a Culture Event",
-  address: "Britannia, Part-Dieu",
-  event_type: "culture",
-  wild: 2,
-  explained_description: "This is a Culture Event in Part-Dieu !",
-  tips: "Stage 2, Bloc B",
-  pending: true,
-  city: "Lyon",
-  secret_event: false
-)
 
 Event.create(
-  name: "Event Culture 2",
-  normal_description: "This is a Culture Event 2",
-  address: "Britannia, Part-Dieu",
-  event_type: "culture",
-  wild: 2,
-  explained_description: "This is a Culture Event 2 in Part-Dieu !",
-  tips: "Stage 2, Bloc B",
+  name: "Behind the Hotel, come, dance and speak spanish! ",
+  normal_description: "Underground bar where we rediscover the Argentine culture. La Mezcaleria is a very secret place, hidden behind the kitchens of the hotel.",
+  address: "4 rue André Bollier, 69008 Lyon",
+  event_type: "drink",
+  wild: 6,
+  explained_description: "Maximum two people accepted.",
+  tips: "You have to know how to say 'Your bar is the most special bar in this city' in spanish.",
   pending: true,
   city: "Lyon",
   secret_event: false
 )
 
-Event.create(
-  name: "Event Culture 3",
-  normal_description: "This is a Culture Event 3",
-  address: "Britannia, Part-Dieu",
-  event_type: "culture",
-  wild: 2,
-  explained_description: "This is a Culture Event 3 in Part-Dieu !",
-  tips: "Stage 2, Bloc B",
-  pending: true,
-  city: "Lyon",
-  secret_event: false
-)
-
-Event.create(
-  name: "Event Culture 4",
-  normal_description: "This is a Culture Event 4",
-  address: "Britannia, Part-Dieu",
-  event_type: "culture",
-  wild: 2,
-  explained_description: "This is a Culture Event 4 in Part-Dieu !",
-  tips: "Stage 2, Bloc B",
-  pending: true,
-  city: "Lyon",
-  secret_event: false
-)
-
-Event.create(
-  name: "Event Culture 5",
-  normal_description: "This is a Culture Event 5",
-  address: "Britannia, Part-Dieu",
-  event_type: "culture",
-  wild: 2,
-  explained_description: "This is a Culture Event 5 in Part-Dieu !",
-  tips: "Stage 2, Bloc B",
-  pending: true,
-  city: "Lyon",
-  secret_event: false
-)
+#------------
 
 puts "
 _░▒███████
