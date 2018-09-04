c93507f85e9ede2e02aaa1cd447ef6fc8a432264
benjamin <benjolivot@gmail.com>
2018-09-04 11:49:13 +0200

    modif events seed schema

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,25 +7,7 @@ class EventsController < ApplicationController
 
   def index
     @choices = current_user.choices.where(accepted: true)
-     @events = []
-     @events << @choices[0].event
-     @events << @choices[1].event
-     @events << @choices[2].event
-
-
-    # @events = @choices.where.not(latitude: nil, longitude: nil)
-
-    iconBase = ActionController::Base.helpers.asset_path('localisation.png')
-
-    @markers = @events.map do |event|
-      {
-        lat: event.latitude,
-        lng: event.longitude,
-        icon: iconBase
-      }
-
-    end
-
+    @events = @choices.map { |choice| choice.event }
   end
 
 
