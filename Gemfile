d80d35d105e18ffe33bbbeddf39ab480f37110c2
remy duquenne <duquenneremy@gmail.com>
2018-09-05 12:32:14 +0200

    maj btn wild

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'puma'
 gem 'rails', '5.2.1'
 gem 'redis'
 gem "geocoder"
-
+gem 'flutie'
 
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass', '~> 3.3'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def collection_for_wildness
     [
-      [image_tag('wild1.png'), 1],
-      [image_tag('wild2.png'), 2],
-      [image_tag('wild3.png'), 3],
-      [image_tag('wild4.png'), 4]
+      [image_tag('wild1.png') + "<p>lorem ipsum 1</p>".html_safe, 1],
+      [image_tag('wild2.png') + "<p>lorem ipsum 2</p>".html_safe, 2],
+      [image_tag('wild3.png') + "<p>lorem ipsum 3</p>".html_safe, 3],
+      [image_tag('wild4.png') + "<p>lorem ipsum 4</p>".html_safe, 4]
     ]
   end
 end
