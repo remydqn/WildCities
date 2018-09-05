e48f1be481a9914a5a79ac52a6b3d8bf3939b0be
remy duquenne <duquenneremy@gmail.com>
2018-09-05 10:51:52 +0200

    btn wild ok

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def collection_for_wildness
+    [
+      [image_tag('wild1.png'), 1],
+      [image_tag('wild2.png'), 2],
+      [image_tag('wild3.png'), 3],
+      [image_tag('wild4.png'), 4]
+    ]
+  end
 end
