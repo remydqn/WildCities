2adb8ec1428e81029976ca233cec0535ad62cb0b
remy duquenne <duquenneremy@gmail.com>
2018-09-05 16:52:42 +0200

    modif taille icon wild + color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def collection_for_wildness
     [
-      [image_tag('wild1.png') + "<p>lorem ipsum 1</p>".html_safe, 1],
-      [image_tag('wild2.png') + "<p>lorem ipsum 2</p>".html_safe, 2],
-      [image_tag('wild3.png') + "<p>lorem ipsum 3</p>".html_safe, 3],
-      [image_tag('wild4.png') + "<p>lorem ipsum 4</p>".html_safe, 4]
+      [image_tag('wild1.png') + "<p>LEVEL 1 : DUCK</p>".html_safe, 1],
+      [image_tag('wild2.png') + "<p>LEVEL 2 : PENGUIN</p>".html_safe, 2],
+      [image_tag('wild3.png') + "<p>LEVEL 3 : WOLF</p>".html_safe, 3],
+      [image_tag('wild4.png') + "<p>LEVEL 4 : LION</p>".html_safe, 4]
     ]
   end
 end
